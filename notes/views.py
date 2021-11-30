de853a9a2d5bf0b8fd0824665fde04b4766542f5
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-11-30 12:15:01 +0100

    Add notes urls, views and templates

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Notes
+
 # Create your views here.
+def list(request):
+    notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': notes})
