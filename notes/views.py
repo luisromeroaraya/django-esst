d02d5b514b2a5a7a7a69140e4161b411fe8a6ac2
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-01 22:03:57 +0100

    Added foreign key to the notes model to establish a relationship between the user table and the notes table

--- notes/views.py
+++ notes/views.py
@@ -1,31 +1,48 @@
+from typing import List
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.http import Http404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Notes
 from .forms import NotesForm
 
 # Create your views here.
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
+    login_url = '/admin'
 
-class NotesUpdateView(UpdateView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
+    login_url = '/admin'
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = '/admin'
 # def list(request):
 #     notes = Notes.objects.all()
 #     return render(request, 'notes/notes_list.html', {'notes': notes})
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 # def detail(request, pk):
 #     try:
@@ -33,7 +50,8 @@ class NotesListView(ListView):
 #     except Notes.DoesNotExist:
 #         raise Http404("Note doesn't exist")
 #     return render(request, 'notes/notes_detail.html', {'note': note})
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = 'note'
     template_name = 'notes/notes_detail.html'
+    login_url = '/admin'
