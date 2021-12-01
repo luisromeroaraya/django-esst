5f2d94f8f797f5aa89db3c40c4bb935207d99cc1
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-01 21:19:04 +0100

    Add forms.py to note app

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('notes', views.NotesListView.as_view(), name='notes.list'),
     # path('notes/<int:pk>', views.detail),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'),
+    path('notes/create', views.NotesCreateView.as_view(), name='notes.create'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 from .models import Notes
+from .forms import NotesForm
 
 # Create your views here.
+class NotesCreateView(CreateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
 
 # def list(request):
 #     notes = Notes.objects.all()
 #     return render(request, 'notes/notes_list.html', {'notes': notes})
-
 class NotesListView(ListView):
     model = Notes
     context_object_name = 'notes'
@@ -20,7 +24,6 @@ class NotesListView(ListView):
 #     except Notes.DoesNotExist:
 #         raise Http404("Note doesn't exist")
 #     return render(request, 'notes/notes_detail.html', {'note': note})
-
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = 'note'
